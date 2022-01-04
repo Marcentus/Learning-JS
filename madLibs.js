7fc54f3990bb68334515f646ea0382a76ebfc4c5
Thomas Bouldin <tbouldinc@gmail.com>
2022-01-04 13:23:23 -0500

    created madLibs.js

---
+++ madLibs.js
@@ -0,0 +1,7 @@
+
+function madLibs(noun, verb, adjective, adverb) {
+    var result = "The " + noun + " " + verb + " the " + adjective + " bus " + adverb;
+    return result
+}
+
+console.log(madLibs("Jack", "slapped", "big", "quickly"));
